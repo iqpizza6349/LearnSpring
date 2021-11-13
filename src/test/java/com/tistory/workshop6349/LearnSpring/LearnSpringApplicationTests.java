82640655b0bcf8f22155a47629ba26872423ac19
iqpizza6349 <iqpizza6349@gmail.com>
2021-11-13 11:56:23 +0900

    Initial commit

---
+++ src/test/java/com/tistory/workshop6349/LearnSpring/LearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.workshop6349.LearnSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
