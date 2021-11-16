f8e6b3e0b249a1c3e735fa62fd7de1811f6d8426
iqpizza6349 <iqpizza6349@gmail.com>
2021-11-16 14:20:39 +0900

    MVC

--- src/main/java/com/tistory/workshop6349/LearnSpring/controller/HelloController.java
+++ src/main/java/com/tistory/workshop6349/LearnSpring/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.tistory.workshop6349.LearnSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,11 @@ public class HelloController {
         model.addAttribute("data","Hello!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
+
 }
